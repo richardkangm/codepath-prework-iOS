5c7278981dff1d0be0e9cb24a10c98b701f03155
richardkangm <58668877+richardkangm@users.noreply.github.com>
2022-08-01 18:16:34 -0500

    Replaced with sliders and fixed constraints

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelTipAmount: UILabel!
     @IBOutlet weak var controlTipPercent: UISegmentedControl!
     @IBOutlet weak var labelTotal: UILabel!
+    @IBOutlet weak var labelSliderValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,18 +24,32 @@ class ViewController: UIViewController {
         
         // Get bill amount from text field input
         let bill = Double(txtBillAmount.text!) ?? 0
-        
+        let sliderTipAmount = Double(labelSliderValue.text!) ?? 0
         // Get total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tip = bill *
-        tipPercentages[controlTipPercent.selectedSegmentIndex]
-        let total = bill + tip
+        // let tipPercentages = [0.15, 0.18, 0.2]
+        // let tip = bill *
+        // tipPercentages[controlTipPercent.selectedSegmentIndex]
+        
+        // let total = bill + tip
+        
+        let sliderTip = bill * sliderTipAmount / 100
+        let sliderTotal = bill + sliderTip
+        
         
         // Update tip amount label
-        labelTipAmount.text = String(format: "$%.2f", tip)
+        labelTipAmount.text = String(format: "$%.2f", sliderTip)
         
         // Update total amount
-        labelTotal.text = String(format: "$%.2f", total)
+        // labelTotal.text = String(format: "$%.2f", total)
+        
+        // Update total amount for slider
+        labelTotal.text = String(format: "$%.2f", sliderTotal)
+    }
+    
+    @IBAction func slider(_ sender: UISlider) {
+        let value = sender.value
+        labelSliderValue.text = String(format: "%.1f" ,value)
+        
     }
     
     
